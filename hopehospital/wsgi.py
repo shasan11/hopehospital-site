443f81e7b7758321b5445cca272159e52ab0534a
Shasan Dhakal <shasandkl4@gmail.com>
2026-01-25 19:12:13 +0545

    production final for wsgi.py

--- hopehospital/wsgi.py
+++ hopehospital/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for hopehospital project.
+WSGI config for freightmaster project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hopehospital.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'freightmaster.settings')
 
 application = get_wsgi_application()
+app=application
